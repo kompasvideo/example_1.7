169308a4bc15c7f7f0191fae3559e55c57f0923a
Андрей Барышников <andrey1andrey@narod.ru>
2023-09-27 15:24:13 +0300

    3

--- src/main/java/com/example/users/model/User.java
+++ src/main/java/com/example/users/model/User.java
@@ -2,10 +2,14 @@ package com.example.users.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 @AllArgsConstructor
@@ -13,6 +17,13 @@ import lombok.NoArgsConstructor;
 @Data
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String email;
 }
